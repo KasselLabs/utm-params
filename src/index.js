4d22d98727a8f3a097cd3d71d51afc8767c325fc
Nihey Takizawa <nihey.takizawa@gmail.com>
2021-07-07 12:00:03 -0300

    Remove initial params saving on src/index.js

--- src/index.js
+++ src/index.js
@@ -8,18 +8,8 @@ const allowedParams = [
   "utm_content",
   "utm_name",
   "utm_term",
-  "initial_utm_source",
-  "initial_utm_medium",
-  "initial_utm_campaign",
-  "initial_utm_content",
-  "initial_utm_name",
-  "initial_utm_term",
   "gclid",
 ];
-
-function checkIfInitialParamsExist(params) {
-  return Object.keys(params).find(k => k.includes("initial"));
-};
 
 let storage = null;
 
@@ -63,36 +53,7 @@ class UTMParams {
       return false;
     }
     try {
-      const paramsToSave = {};
-      const initialParams = {};
-
-      Object.assign(paramsToSave, params);
-
-      if (storage.getItem("utmSavedParams")) {
-        let existingParams = {};
-
-        try {
-          existingParams = JSON.parse(storage.getItem("utmSavedParams"));
-        } catch (e) {
-          existingParams = {};
-        }
-
-        if (checkIfInitialParamsExist(existingParams)) {
-          Object.keys(existingParams).forEach(k => {
-            if(k.includes('initial_')) {
-              initialParams[k] = existingParams[k];
-            }
-          });
-        }
-      } else {
-        Object.keys(paramsToSave).forEach(k => {
-          initialParams['initial_'+k] = paramsToSave[k];
-        });
-      }
-
-      Object.assign(paramsToSave, initialParams);
-
-      storage.setItem("utmSavedParams", JSON.stringify(paramsToSave));
+      storage.setItem("utmSavedParams", JSON.stringify(params));
       return true;
     } catch (e) {
       throw new Error(e);
