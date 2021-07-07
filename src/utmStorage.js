5d0f2b7fa8413a9e978bc15a4c6a4ec82fb1a2ef
Nihey Takizawa <nihey.takizawa@gmail.com>
2021-07-07 10:56:31 -0300

    Fix utmStorage not being able to save the params in localStorage

--- src/utmStorage.js
+++ src/utmStorage.js
@@ -1,14 +1,14 @@
 class UTMStorage {
   constructor() {
+    this.prefixToAvoidBuiltIns = '_utm_unique_';
     try {
       const ls = window.localStorage;
-      this.setItem = ls.setItem;
-      this.getItem = ls.getItem;
-      this.removeItem = ls.removeItem;
+      this.setItem = (key, value) => ls.setItem(this.prefixToAvoidBuiltIns + key, value);
+      this.getItem = (key) => ls.getItem(this.prefixToAvoidBuiltIns + key);
+      this.removeItem = (key) => ls.removeItem(this.prefixToAvoidBuiltIns + key);
       this.setItem('test', 'test');
       this.removeItem('test');
     } catch(e) {
-      this.prefixToAvoidBuiltIns = '_utm_unique_';
       this.localMockedStorage = {};
       this.setItem = this.setLocalItem;
       this.getItem = this.getLocalItem;
